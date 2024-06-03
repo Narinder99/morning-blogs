2ece19dc73453a147d1651fcb21a7dae489c7728
Narinder Singh <50241887+Narinder99@users.noreply.github.com>
2024-06-04 00:50:33 +0530

    dummy project

---
+++ src/components/Blog/ViewCounter.js
@@ -0,0 +1,71 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React, { useEffect, useState } from "react";
+
+const supabase = createClientComponentClient({
+  supabaseUrl: "https://riovqygztztjkkufnnsv.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb3ZxeWd6dHp0amtrdWZubnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNTU5MjcsImV4cCI6MjAzMjczMTkyN30.PYn4137qzKJ8WXNI-ckXnnpH68mBjvOXfKAhApy4R5s",
+});
+
+const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
+  const [views, setViews] = useState(0);
+
+  useEffect(() => {
+    const incrementView = async () => {
+      try {
+        let { error } = await supabase.rpc("increment", {
+          slug_text:slug ,
+        });
+
+        if (error){
+            console.error("Error incrementing view count inside try block:", error)
+        };
+        
+      } catch (error) {
+        console.error(
+          "An error occurred while incrementing the view count:",
+          error
+        );
+      }
+    };
+
+    if(!noCount){
+        incrementView();
+    }
+  }, [slug, noCount]);
+
+  useEffect(() => {
+    const getViews = async () => {
+      try {
+        let { data, error } = await supabase
+  .from('views')
+  .select('count')
+  .match({slug: slug})
+  .single()
+
+        if (error){
+            console.error("Error incrementing view count inside try block:", error)
+        };
+
+
+        setViews(data ? data.count : 0)
+        
+      } catch (error) {
+        console.error(
+          "An error occurred while incrementing the view count:",
+          error
+        );
+      }
+    };
+
+        getViews();
+  }, [slug]);
+
+  if (showCount) {
+    return <div>{views} views</div>;
+  } else {
+    return null;
+  }
+};
+
+export default ViewCounter;
