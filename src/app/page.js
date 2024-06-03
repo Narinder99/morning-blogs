04b59f2686d9082914c087cfc9e52757b129f8fe
Narinder Singh <50241887+Narinder99@users.noreply.github.com>
2024-06-04 03:07:10 +0530

    add slogen and modify header

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@ import {allBlogs} from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
+import OneLineSlogan from "../components/Home/OneLineSlogan";
 
 export default function Home() {
   
   return (
     <main className="flex flex-col items-center justify-center">
+      <OneLineSlogan />
       <HomeCoverSection blogs={allBlogs} />
-      {/* <FeaturedPosts blogs={allBlogs} />
-      <RecentPosts blogs={allBlogs} /> */}
+      <FeaturedPosts blogs={allBlogs} />
+      {/* <RecentPosts blogs={allBlogs} /> */}
 
 
     </main>
